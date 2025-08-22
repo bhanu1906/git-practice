4faf5876506ad9f36defede57900f48ef9648f4b
payyalabhanu <payyalabhanu1999@gmail.com>
2025-08-22 20:20:53 +0530

    variables

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# index start from 0,size is 3
+FRUITS=("APPLE" "KIWI" "ORANGE") #Array
+echo "frist fruit is: ${FRUIT[0]}"
+echo "second fruit is: ${FRUIT[1]}"
+echo "third fruit is: ${FRUIT[2]}"
+
+
+echo "frist fruit is: ${FRUIT[@]}"
+
