a8f07e102d82a3144f15927bd4575b07d33d3654
payyalabhanu <payyalabhanu1999@gmail.com>
2025-08-26 17:58:29 +0530

    functions

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 USERID=$(id -u)
-echo "user ID is: $USERID"
+#echo "user ID is: $USERID"
+
+if [ $USERID -ne 0 ]
+then
+    echo "please  run this script with root priveleges"
+
+fi
+
+dnf install git -y
