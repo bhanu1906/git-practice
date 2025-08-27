ae1b5d9ec90a1523aaff15e5413989cd13c23872
payyalabhanu <payyalabhanu1999@gmail.com>
2025-08-27 10:49:26 +0530

    shell

--- 15-redirectors.sh
+++ 15-redirectors.sh
@@ -10,6 +10,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
@@ -37,10 +38,10 @@ do
      dnf list installed $package
      if [ $? -ne 0 ]
      then
-         echo "$package is not  installed, going to install it.."
-         dnf install $package -y
+         echo "$package is not  installed, going to install it.." &>>$LOG_FILE
+         dnf install $package -y &>>$LOG_FILE
          VALIDATE $? "installing $package"
     else
-        echo "$package is already installed..nothing to do"
+        echo "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
     fi
 done
